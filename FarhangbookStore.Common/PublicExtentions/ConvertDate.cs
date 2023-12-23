91ea8140fa80a2e461daa1d037b03c0e18b43908
Hamed Shariati Mehr <nextlab.info@gmail.com>
2023-12-23 21:53:52 +0330

    Add project files.

---
+++ FarhangbookStore.Common/PublicExtentions/ConvertDate.cs
@@ -0,0 +1,24 @@
+﻿using MD.PersianDateTime.Standard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarhangbookStore.Common.PublicExtentions
+{
+	public class ConvertDate
+	{
+		public DateTime ConvertShamsiToMiladi(string Date)
+		{
+			PersianDateTime persianDateTime = PersianDateTime.Parse(Date);
+			return persianDateTime.ToDateTime();
+		}
+
+		public string ConvertMiladiToShamsi(DateTime Date, string Format)
+		{
+			PersianDateTime persianDateTime = new PersianDateTime(Date);
+			return persianDateTime.ToString(Format);
+		}
+	}
+}
---
+++ FarhangbookStore/Program.cs
@@ -0,0 +1,81 @@
+﻿using FarhangbookStore.Common.PublicExtentions;
+using NToastNotify;
+using WebMarkupMin.AspNetCore7;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+#region سرویس مربوط به تاریخ شمسی
+builder.Services.AddTransient<ConvertDate>();
+#endregion
+
+#region سرویس مربوط به بهینه سازی صفحات در پروژه
+//WebMarkupMin.AspNetCore7  : استفاده از پکیج کاربری مخصوص ورژن 7 دات نت کور
+//NuGet\Install-Package WebMarkupMin.AspNetCore7 -Version 2.14.1
+builder.Services.AddWebMarkupMin(option =>
+{
+	option.AllowCompressionInDevelopmentEnvironment = true;
+	option.AllowMinificationInDevelopmentEnvironment = true;
+}).AddHtmlMinification(
+			options =>
+			{
+				options.MinificationSettings.RemoveRedundantAttributes = true;
+				options.MinificationSettings.RemoveHttpProtocolFromAttributes = true;
+				options.MinificationSettings.RemoveHttpsProtocolFromAttributes = true;
+			})
+  .AddHttpCompression();
+
+#endregion
+
+#region PWA سرویس مربوط به
+//builder.Services.AddProgressiveWebApp();
+
+#endregion
+
+#region متد های مربوط به نوتیفیکیشن
+builder.Services.AddMvc().AddNToastNotifyToastr(new ToastrOptions()
+{
+	ProgressBar = true,
+	PositionClass = ToastPositions.TopCenter
+});
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseWebMarkupMin();
+
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseNToastNotify();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "Administrator",
+	pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+	name: "Users",
+	pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
